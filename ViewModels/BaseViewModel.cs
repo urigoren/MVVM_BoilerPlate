b42ad7e9614691a6c4be9fbdba4c7b5fb66dc1d9
Uri Goren <uri@goren4u.com>
2014-01-15 11:27:50 +0200

    Update BaseViewModel.cs

--- ViewModels/BaseViewModel.cs
+++ ViewModels/BaseViewModel.cs
@@ -28,23 +28,6 @@ namespace MVVM_BoilerPlate.ViewModels
             return true;
         }
 
-        protected virtual void OnPropertyChanged<T>(Expression<Func<T>> selectorExpression)
-        {
-            if (selectorExpression == null)
-                throw new ArgumentNullException("selectorExpression");
-            MemberExpression body = selectorExpression.Body as MemberExpression;
-            if (body == null)
-                throw new ArgumentException("The body must be a member expression");
-            OnPropertyChanged(body.Member.Name);
-        }
-
-        protected bool SetField<T>(ref T field, T value, Expression<Func<T>> selectorExpression)
-        {
-            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-            field = value;
-            OnPropertyChanged(selectorExpression);
-            return true;
-        }
         #endregion
         #region Command Helpers
         internal RelayCommand CommandFromFunction(Action<object> func)
